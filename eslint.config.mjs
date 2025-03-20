057b9fad8873166f21221b47fb4dcf05ebb1db8f
FedeWaelk <fedewaelk@gmail.com>
2025-03-20 14:22:26 -0300

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...configPrettier.rules,
+      "prettier/prettier": ["error", { "endOfLine": "auto" }],
+    },
+  },
 ];
